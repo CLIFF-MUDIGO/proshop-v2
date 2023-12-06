c4a8c629cd5e3648503f3fbd2941ee9fec07050c
cliff <cliffmudigo5@gmail.com>
2023-12-06 17:55:34 +0300

    :sparkles: feat: header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
-
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <h1>Welcome to proShop</h1>
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
+      <h1>Welcome to proShop</h1>
+      </Container>
+    
+    </main>
+   <Footer />
+    </>
+    
   )
   
 }
