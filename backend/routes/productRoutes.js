0e679cec57913737f942cb4bda96e85f36c3c5d1
cliff <cliffmudigo5@gmail.com>
2023-12-11 13:20:30 +0300

    backend partialy done

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,9 @@
 import express from 'express';
 const router = express.Router();
-import asyncHandler from '../middleware/asyncHandler.js';
-import Product from '../models/productModel.js';
+import { getProducts, getProductsById } from '../controllers/productController.js';
 
-router.get("/", asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-}))
+router.route('/').get(getProducts);
 
-router.get("/:id", asyncHandler(async (req, res) => {
-    const productId = req.params.id;
-
-    console.log("Product ID:", productId);
-
-    if (!productId) {
-        return res.status(400).json({
-            message: "Invalid product ID"
-        });
-    }
-
-    const product = await Product.findById(productId);
-
-    if (product) {
-        return res.json(product);
-    }
-
-    res.status(404).json({
-        message: "Product not found"
-    });
-}));
-
+router.route('/:id').get(getProductsById);
 
 export default router;
