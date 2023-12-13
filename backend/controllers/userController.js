9a9cc52bba0f669138b4c21d2e00a298401a218c
cliff <cliffmudigo5@gmail.com>
2023-12-13 13:26:21 +0300

    :lock: user Register Endpoint and encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
+import generateToken from '../utilis/generateToken.js';
 
 
 const authUser = asyncHandler(async (req, res) => {
@@ -9,27 +10,45 @@ const authUser = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email });
 
     if (user && (await user.matchPassword(password))) {
-        const token = jwt.sign({ userId: user._id, isAdmin: user.isAdmin }, process.env.JWT_SECRET, {
-            expiresIn: '30d',
-        });
-
-        res.cookie('jwt', token, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV !== 'development',
-            sameSite: 'strict',
-            maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-        });
-
+       generateToken(res, user._id);
         res.json({ _id: user._id, name: user.name, email: user.email, isAdmin: user.isAdmin });
     } else {
         res.status(401).json({ message: 'Invalid email or password' });
     }
 });
-
+//desc: Register a new user
+//route: POST /api/users/register
+//access: Public
 const registerUser = asyncHandler(async (req, res) => {
-    // Your registration logic goes here
-    res.send('register user');
-});
+    const { name, email, password } = req.body;
+
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400);
+        throw new Error('User already exists');
+    }
+    const user = await User.create({
+        name,
+        email,
+        password,
+    });
+     if (user){
+        generateToken(res, user._id);
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            
+        });
+     }else{
+        res.status(400);
+        throw new Error('Invalid user data');
+     }
+    });
+
 //desc: Logout user / clear cookie
 //route: POST /api/users/logout
 //access: Private
