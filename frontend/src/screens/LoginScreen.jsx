5094faec701687bd50658f7758c5190b1e6d685d
cliff <cliffmudigo5@gmail.com>
2023-12-13 23:24:36 +0300

    :lock:Sign up functionality

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -84,7 +84,8 @@ const LoginScreen = () => {
             </Form>
             <Row className="py-3">
                 <Col>
-                New Customer? <Link to={redirect ? `register?redirect=${redirect}` : '/register'}>Register</Link>
+                New Customer? <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+
 
                 </Col>
             </Row>
